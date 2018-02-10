ca883c9416930417d0e265e981374acdaad5f311
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-02-10 18:42:54 +0100

    use builder adapter

--- src/LineUpElement.ts
+++ src/LineUpElement.ts
@@ -1,12 +1,27 @@
 const { customElement, property, query, observe } = Polymer.decorators;
+import {
+  builderAdapter,
+  Column,
+  IBuilderAdapterProps,
+  ICellRendererFactory,
+  IDynamicHeight,
+  IGroupData,
+  IGroupItem,
+  IToolbarAction,
+  LineUp,
+  Taggle,
+  ILineUpOptions,
+  LocalDataProvider,
+  ITaggleOptions,
+  Ranking,
+} from 'lineupjs';
 import * as css from 'raw-loader!lineupjs/build/LineUpJS.css';
-import { LocalDataProvider, LineUp, builder } from 'lineupjs';
 
 {
   // see https://github.com/Polymer/polymer/issues/2386
   // font-face are not supported in custom styles need to import globally
   const style = String(css.default);
-  const fontFace = style.match(/@font-face[^}]+\}/);
+  const fontFace = style.match(/@font-face[^}]+}/);
   if (fontFace) {
     const s = document.createElement('style');
     s.innerText = fontFace[0];
@@ -14,44 +29,131 @@ import { LocalDataProvider, LineUp, builder } from 'lineupjs';
   }
 }
 
-@customElement('lineup-element')
-export class LineUpElement extends Polymer.Element {
+function debounce(callback: () => void, timeToDelay = 100) {
+  let tm = -1;
+  return () => {
+    if (tm >= 0) {
+      clearTimeout(tm);
+      tm = -1;
+    }
+    tm = self.setTimeout(() => {
+      tm = -1;
+      callback();
+    }, timeToDelay);
+  };
+}
 
-  private provider: LocalDataProvider;
-  private instance: LineUp;
+
+@customElement('lineup-element')
+export class LineUpElement extends Polymer.Element implements IBuilderAdapterProps {
 
   @property({ type: Array })
   data: any[] = [];
 
+  @property({ type: Array })
+  selection: number[] | null = null;
+  @property({ type: Number })
+  highlight: number | null = null;
+
+  onSelectionChanged(selection: number[]) {
+    this.dispatchEvent(new CustomEvent('selectionChanged', {detail: {selection}}));
+  }
+  onHighlightChanged(highlight: number) {
+    this.dispatchEvent(new CustomEvent('highlightChanged', {detail: {highlight}}));
+  }
+
+  @property({ type: Boolean })
+  singleSelection?: boolean;
+  @property({ type: Boolean })
+  filterGlobally?: boolean;
+  @property({ type: Boolean })
+  noCriteriaLimits?: boolean;
+  @property({ type: Number })
+  maxGroupColumns?: number;
+  @property({ type: Number })
+  maxNestedSortingCriteria?: number;
+  columnTypes?: { [type: string]: typeof Column };
+
+  @property({ type: Object })
+  deriveColumns?: boolean | string[];
+  @property({ type: Boolean })
+  deriveColors?: boolean;
+
+  @property({ type: Object })
+  restore?: any;
+  @property({ type: Object })
+  defaultRanking?: boolean | 'noSupportTypes';
+
+  @property({ type: Boolean })
+  animated?: boolean;
+  @property({ type: Boolean })
+  sidePanel?: boolean;
+  @property({ type: Boolean })
+  sidePanelCollapsed?: boolean;
+  @property({ type: String })
+  defaultSlopeGraphMode?: 'item' | 'band';
+  @property({ type: Boolean })
+  summaryHeader?: boolean;
+  @property({ type: Boolean })
+  expandLineOnHover?: boolean;
+  @property({ type: Boolean })
+  overviewMode?: boolean;
+
+  @property({ type: Object })
+  renderer?: { [id: string]: ICellRendererFactory };
+  @property({ type: Object })
+  toolbar?: { [id: string]: IToolbarAction };
+
+  @property({ type: Number })
+  rowHeight?: number;
+  @property({ type: Number })
+  rowPadding?: number;
+
+  @property({ type: Number })
+  groupHeight?: number;
+  @property({ type: Number })
+  groupPadding?: number;
+
+  @property({ type: Object })
+  dynamicHeight?: (data: (IGroupItem | IGroupData)[], ranking: Ranking) => (IDynamicHeight | null);
+
   @query('main')
   private _main: HTMLElement;
 
+  private readonly _adapter = new builderAdapter.Adapter({
+    props: () => this,
+    createInstance: (data: LocalDataProvider, options: Partial<ILineUpOptions>) => this.createInstance(this._main, data, options),
+    columnDescs: (_data: any[]) => [], //filterChildrenProps<LineUpColumnDesc, any>(this.props.children, LineUpColumnDesc).map((d) => d.type.build(d.props, data)),
+    rankingBuilders: () => [], //filterChildrenProps<LineUpRanking>(this.props.children, LineUpRanking).map((d) => LineUpRanking.merge(d.props))
+  });
+  private _changed: Partial<IBuilderAdapterProps>= {};
+
+  private _triggerChanged = debounce(() => this._updateImpl(), 50);
+
+  protected createInstance(node: HTMLElement, data: LocalDataProvider, options: Partial<ITaggleOptions>): LineUp|Taggle {
+    return new LineUp(node, data, options);
+  }
+
+  componentDidMount() {
+    this._adapter.componentMouned();
+  }
+
+  private _updateImpl() {
+    const changed = Object.assign({}, this._changed);
+    this._changed = {};
+    this._adapter.componentDidUpdate((key: keyof IBuilderAdapterProps) => Boolean(changed[key]));
+  }
+
   ready() {
     super.ready();
-
-    if (!this.provider && this.data != null && this.data.length > 0) {
-      this._init();
-    }
+    this._adapter.componentMouned();
   }
 
-  private _init() {
-    this.provider = builder(this.data).deriveColumns().buildData();
-    this.instance = new LineUp(this._main, this.provider);
-  }
 
-  update() {
-    if (this.instance) {
-      this.instance.update();
-    }
-  }
-
-  @observe('data')
-  onDataChanged() {
-    if (this.provider) {
-      this.provider.setData(this.data);
-    } else if (this.data != null && this.data.length > 0) {
-      this._init();
-    }
+  @observe('*')
+  onChanged(changeRecord: {path: keyof IBuilderAdapterProps, value: any}) {
+    this._changed[changeRecord.path] = true;
+    this._triggerChanged();
   }
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
 
 export * from './LineUpElement';
+
+
+declare const __VERSION__: string;
+declare const __BUILD_ID__: string;
+declare const __LICENSE__: string;
+/**
+ * LineUp version
+ * @type {string}
+ */
+export const version = __VERSION__;
+/**
+ * LineUp unique build id
+ * @type {string}
+ */
+export const buildId = __BUILD_ID__;
+/**
+ * LineUp license type
+ * @type {string}
+ */
+export const license = __LICENSE__;
